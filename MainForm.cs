56f1b86bcd4f5e95f3e904e8a65494e4e3651fdb
ghostiek <patricktawil1@hotmail.com>
2016-09-26 08:56:19 -0700

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -1,9 +1,7 @@
+﻿
 using CefSharp;
 using ReDevPro.Components;
 using System;
-using System.Drawing;
-using System.IO;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace ReDevPro
@@ -12,14 +10,13 @@ namespace ReDevPro
     {
         public MainForm()
         {
-            Cef.EnableHighDPISupport();
             InitializeComponent();
-            HideComps();
+            Cef.EnableHighDPISupport();
             Cef.Initialize(new CefSettings()); //required for cefsharp to work, only needs to be called once
 
             //Add web tabs
             WebTab Chat = new WebTab("https://discord.gg/zUqJTYP") { Text = "Chat" };
-            WebTab News = new WebTab("https://ygodevpro.com/forum/index.php/Board/14-Announcements/") { Text = "News" };
+            WebTab News = new WebTab("https://ygodevpro.com/index.php/NewsOverview/") { Text = "News" };
             WebTab BugTracker = new WebTab("https://ygodevpro.com/forum/bugs/") { Text = "BugTracker" };
             IntPtr h = MainTabControl.Handle; //required for insert to work
             MainTabControl.TabPages.Insert(0, Chat);
@@ -28,7 +25,10 @@ namespace ReDevPro
             MainTabControl.SelectedIndex = 0;
 
             //Server Select
-            ServerSelect.Items.Add("LocalHost");
+            Config.AddServer("Butty's Test Server", "81.98.22.127", 8911);
+            Config.AddServer("LocalHost", "127.0.0.1", 8911);
+            ServerSelect.Items.AddRange(Config.GetServerList());
+            ServerSelect.SelectedIndexChanged += UpdateSelectedServer;
             ServerSelect.SelectedIndex = 0;
 
             //options
@@ -40,8 +40,7 @@ namespace ReDevPro
         {
             YGOProOptionsControl.Items.Add("Auto Placement", Config.GetBool("Auto Placement", true));
             YGOProOptionsControl.Items.Add("Auto Chain", Config.GetBool("Auto Chain", true));
-            YGOProOptionsControl.Items.Add("Random Placement", Config.GetBool("Auto-Placement", false));
-            YGOProOptionsControl.Items.Add("Enable User Covers", Config.GetBool("Enable User Covers", true));
+            YGOProOptionsControl.Items.Add("Random Placement", Config.GetBool("Random Placement", false));
             YGOProOptionsControl.Items.Add("No Chain Delay", Config.GetBool("No Chain Delay", false));
             YGOProOptionsControl.Items.Add("Mute Opponents", Config.GetBool("Mute Opponents", false));
             YGOProOptionsControl.Items.Add("Mute Spectators", Config.GetBool("Mute Spectators", false));
@@ -53,11 +52,31 @@ namespace ReDevPro
             YGOProOptionsControl.Items.Add("Enable Sound", Config.GetBool("Enable Sound", true));
             YGOProOptionsControl.Items.Add("Enable Music", Config.GetBool("Enable Music", true));
             YGOProOptionsControl.Items.Add("Enable Direct X", Config.GetBool("Enable Direct X", false));
+
+            //settings that need to be added
+            Config.GetString("nickname", "Tester-" + Program.Random.Next(999));
+            Config.GetInt("sound_volume", 100);
+            Config.GetInt("music_volume", 100);
+            Config.GetString("textfont", "fonts/simhei.ttf 12");
+            Config.GetString("numfont", "fonts/arialbd.ttf");
+            Config.GetInt("skin_index", 0);
         }
 
         private void UpdateOptions(object sender, ItemCheckEventArgs e)
         {
             Config.UpdateBool(YGOProOptionsControl.SelectedItem.ToString(), e.NewValue == CheckState.Checked);
+            Config.SaveConfig();
+        }
+
+        private void UpdateSelectedServer(object sender, EventArgs e)
+        {
+            if (ServerSelect.SelectedIndex != -1)
+            {
+                string selected = ServerSelect.SelectedItem.ToString();
+                Config.UpdateString("lastip", Config.GetServerIP(selected));
+                Config.UpdateString("serverport", Config.GetServerPort(selected).ToString());
+                Config.UpdateString("lastport", Config.GetServerPort(selected).ToString());
+            }
         }
 
         private void DeckEditBtn_Click(object sender, EventArgs e)
@@ -73,55 +92,6 @@ namespace ReDevPro
         private void AiBtn_Click(object sender, EventArgs e)
         {
             YGOProHelper.OpenAI();
-        }
-
-        private void DuelBtn_Click(object sender, EventArgs e)
-        {
-            YGOProHelper.OpenDuel();
-        }
-        private void panel3_Resize(object sender, EventArgs e)
-        {
-            panel3.Height = this.Height;
-        }
-
-        private void Backup_Resize(object sender, EventArgs e)
-        {
-            Backup.Width = this.Width;
-        }
-
-        private void PreviewContent_Click(object sender, EventArgs e)
-        {
-            if(FileList.SelectedItem != null)
-            {
-                //Link to Texture Folder
-                string basePath = @"";
-                pictureBox1.ImageLocation = Path.Combine(basePath, FileList.SelectedItem.ToString());
-            }
-            
-        }
-
-        private void ThemeAdd_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog open = new OpenFileDialog();
-            open.Filter = "Image Files (*.jpg, *.jpeg, *png)|*.jpg;*.jpeg;*.png";
-            open.FilterIndex = 1;
-            DialogResult result = open.ShowDialog();
-
-            if (result == DialogResult.OK)
-            {
-                //Adds image name to file list
-                FileList.Items.Add(open.SafeFileName);
-                pictureBox1.Image = Image.FromFile(open.FileName);
-
-                //Saving the pic. This path will be saved to the texture folder.
-                pictureBox1.Image.Save(@"");
-                FileList.Show();
-            }
-        }
-
-        public void HideComps()
-        {
-            FileList.Hide();
         }
     }
 }
